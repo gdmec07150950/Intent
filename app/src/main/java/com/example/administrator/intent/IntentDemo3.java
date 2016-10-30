27725c9c3a5b02b4b4857d281da41b39dfc4fdd6
yc z <yc z>
2016-10-30 19:05:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/intent/IntentDemo3.java
@@ -0,0 +1,14 @@
+package com.example.administrator.intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/10/29.
+ */
+public class IntentDemo3 extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+}
+}
